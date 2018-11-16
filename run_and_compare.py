2b2cf5afbb9ef4db3a075e0a8225939530ad53ee
Sonja Biedermann <s.bdrmnn@gmail.com>
2018-11-17 00:04:48 +0100

    bla

--- run_and_compare.py
+++ run_and_compare.py
@@ -10,14 +10,15 @@ from sklearn.metrics import normalized_mutual_info_score
 
 f = sys.argv[1]
 csv = pd.read_csv(f, delim_whitespace=True, comment='#', header=None)
-colors = ["b", "r", "g", "grey"]
+
+colors = ["b", "r", "g", "y"]
 
 mat = np.matrix(csv.values[:, 0:2], dtype=np.float32)
 
-clusters, seeds, vectors = orclus.orclus(mat, 3, 2)
+clusters, seeds, vectors = orclus.orclus(mat, 2, 2, k0=50)
 pred = orclus.predict(mat, seeds, vectors)
 
-nmi = normalized_mutual_info_score(csv[2], pred)
+nmi = normalized_mutual_info_score(csv[csv.shape[1] - 1], pred)
 print(f"{'='*32}\nNMI score: {nmi}")
 
 plt.scatter(np.asarray(mat[:, 0]), np.asarray(mat[:, 1]), c=list(map(lambda x: colors[x], pred)))
--- show_data.py
+++ show_data.py
@@ -3,10 +3,10 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 
-col = { "cluster1": "b", "cluster2": "r", "cluster3": "g", "noise": "grey" }
+col = { "cluster1": "b", "cluster2": "r", "cluster3": "g", "cluster4": "y" }
 
 f = sys.argv[1]
 data = pd.read_csv(f, delim_whitespace=True, comment='#', header=None)
 
-plt.scatter(data[0], data[1], c=list(map(lambda x: col[x], data[2])))
+plt.scatter(data[0], data[1], c=list(map(lambda x: col[x], data[data.shape[1] - 1])))
 plt.show()
