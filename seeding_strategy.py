325faaf7929456d1d78cf596fabb36578222afa1
Sonja Biedermann <s.bdrmnn@gmail.com>
2018-11-16 17:06:26 +0100

    use 2 norm

--- seeding_strategy.py
+++ seeding_strategy.py
@@ -9,7 +9,7 @@ def kmeanspp(DB, k):
     seeds = [DB[np.random.randint(len(DB))]]
 
     while len(seeds) < k:
-        Di_sq = [min([linalg.norm(pt - seed)**2 for seed in seeds]) for pt in DB]
+        Di_sq = [min([linalg.norm(pt - seed, 2)**2 for seed in seeds]) for pt in DB]
         sum_Di_sq = sum(Di_sq)
         pr = list(map(lambda x: x / sum_Di_sq, Di_sq))
 
